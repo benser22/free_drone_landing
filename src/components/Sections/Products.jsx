f46352970b472eb310f332f61c63adff855d89e1
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-05-20 10:48:36 -0300

    Change(Products): 3 products show

--- src/components/Sections/Products.jsx
+++ src/components/Sections/Products.jsx
@@ -9,7 +9,7 @@ function Products() {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
